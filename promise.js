797dac444f2c6bafdd8ef122f82d9f51a53d30e8
emyekah <leadload6@gmail.com>
2022-06-12 16:15:20 +0700

    promise

--- promise.js
+++ promise.js
@@ -2,33 +2,27 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 
-const promiseOutput = async (emosi)=>{
-  const ptIix = await promiseTheaterIXX();
-  const ptVgc = await promiseTheaterVGC();
+const promiseOutput =(emosi)=>{
+  return new Promise((res)=>{
+      let keseluruhan=0;
 
-  return new Promise(
-    (res)=>{
-      let hasil;
-
-      const iixMarah;
-      const vgcMarah
-      const iixTidakmarah;
-      const vgcTidakmarah
-      if(emosi == "marah"){
-        iixMarah=ptIix.filter((element)=>element.hasil == "marah");
-        vgcMarah=ptVgc.filter((element)=>element.hasil == "marah");
-
-        hasil = iixMarah + vgcMarah;
-      }else if(emosi == "tidak marah"){
-        iixTidakmarah = ptIix.filter((element)=>element.hasil == "tidak marah");
-        vgcTidakmarah = ptVgc.filter((element)=>element.hasil == "tidak marah");
-        hasil = iixTidakmarah + vgcTidakmarah;
-      }
-
-      return res(hasil);
+      promiseTheaterIXX().then((hasilNonton)=>{
+        hasilNonton.forEach(element => {
+          if (element.hasil === emosi) {
+            keseluruhan++;
+          }
+        });
+      }).then(promiseTheaterVGC().then((hasilNonton)=>{
+        hasilNonton.forEach((element)=>{
+          if (element.hasil == emosi) {
+            keseluruhan++;
+          }
+        })
+        res(keseluruhan);
+      }))
     }
-  )
-};
+  );
+}
 
 module.exports = {
   promiseOutput,
